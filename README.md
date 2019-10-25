ab482debdfe3c05d913f1a21b92a09c086e5bb87
Jan Kiene <jankiene@mailbox.org>
2019-10-25 12:20:43 +0200

    Fix bug in power normalisation

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Vector base amplitude panning (VBAP) implemented in python. Implementation follo
 
 ### Contents
 The algorithm is implemented as a package called `pyvbap` (duh!) that can be installed with `pip install .`(when in this directory). Additionally, there is the `example` folder, that contains a small example application that consist of a class `VbapPlayer` and GUI. The player class loops a given mono audio file and renders the panned signals for a given speaker setup based on the (for now only azimuth) panning angle. Angle can be changed during playback. The GUI visualizes the whole thing and offers panning by clicking.
+There also is a helper script `spkr_setup.py` that offers a dialog for creating a json file for your custom speaker setup.
 
 ### References
 [1]Pulkki, V.: _Virtual Sound Source Positioning Using Vector Base Amplitude Panning_. In: _Journal of the Audio Engineering Society_, Vol. 45 No. 6, 1997
--- pyvbap/vbap.py
+++ pyvbap/vbap.py
@@ -1,18 +1,27 @@
+"""
+Implementation of the VBAP method for panning a source in (almost) arbitrary
+speaker setups.
+"""
 import numpy as np
 
 
 DEG_2_RAD = np.pi / 180
 
 
-def pan_2d(az, s, spkr_pos, base_vecs):
+def pan_2d(az, s, spkr_pos, base_vecs, vol_norm=1):
     """
     Pan the given signal to an azimuth angle in a 2d speaker setup
 
     az - integer azimuth panning angle
     s - signal as numpy array
-    spkr_pos - sorted (!) list of the speaker angles
-    base_vecs - unit vectors pointing to the speakers in same order as spkr_pos
+    spkr_pos - list of the speaker angles (as integers)
+    base_vecs - unit vectors pointing to the speakers in same order (!) as spkr_pos
+    vol_norm - normalisation factor for the computed gains 
     """
+    ret = np.zeros((len(s), len(spkr_pos)))
+    if vol_norm == 0:
+        return ret
+
     # compute unit vector for given angle
     rad = az * DEG_2_RAD
     vec = np.asarray([-np.sin(rad), np.cos(rad)])
@@ -24,24 +33,29 @@ def pan_2d(az, s, spkr_pos, base_vecs):
     active_idx = [x for _, x in sorted(zip(distance, pos_idx))][:2]
     if distance[active_idx[0]] == 0:
         active_idx = active_idx[:1]
-        gains = np.asarray([1])
+        gains = np.asarray([np.sqrt(vol_norm)])
     else:
         inv = np.linalg.inv(base_vecs[:, active_idx])
-        gains = _normalize_gains(inv @ vec, 1)
+        gains = _normalize_gains(inv @ vec, vol_norm)
 
-    ret = np.zeros((len(s), len(spkr_pos)))
+    # return array with shape (len(s), len(spkr_pos))
+    # all columns but those corresponding to the active speakers are zero
     for i in range(len(active_idx)):
         tmp = s * gains[i]
         ret[:, active_idx[i]] = tmp
+
     return ret
 
 
 def _normalize_gains(gains, vol_norm):
     """
-    Normalize gain factors to garantue power conservation
+    Normalize gain factors to garantue power conservation.
     """
     return gains * np.sqrt(vol_norm / np.sum(gains ** 2))
 
 
 def comp_vec_for_angle(ang):
+    """
+    Calculate a vector pointing to the point on a unit circle at angle <ang>
+    """
     return np.asarray([-np.sin(ang), np.cos(ang)])
