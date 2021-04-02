78e1db906610837211e19b759cdd7f7976ab015a
Jan Kiene <jankiene@mailbox.org>
2021-04-02 23:03:57 +0200

    add active triangle selection

--- pyvbap/vbap_panner.py
+++ pyvbap/vbap_panner.py
@@ -5,6 +5,8 @@ speaker setups.
 import numpy as np
 from numpy.typing import ArrayLike
 from typing import Union
+from itertools import combinations
+from scipy.spatial import ConvexHull
 
 
 DEG_2_RAD = np.pi / 180
@@ -15,6 +17,7 @@ class VbapPanner:
         self.ls_az = np.asarray(ls_az, dtype=float)
         self.ls_el = np.asarray(ls_el, dtype=float)
         self.ls_vec = ang_to_cart(self.ls_az, self.ls_el)
+        self.triangles = ConvexHull(self.ls_vec.T).simplices
 
     def calc_gains(self, az: float, el: float, triplet_base: np.ndarray) -> np.ndarray:
         """
@@ -36,10 +39,18 @@ class VbapPanner:
 
     def find_active_triangle(self, az: float, el: float):
         """
-
+        Find active triangle by looping over all possible triangles and choosing
+        the triangle with all positive gains.
         """
-        pass
+        active_tri = np.asarray([-1, -1, -1])
+        for tri in self.triangles:
+            base = self.ls_vec[:, tri]
+            gains = self.calc_gains(az, el, base)
+            if (np.min(gains) > 0):
+                active_tri = tri
+                break
 
+        return active_tri
 
 
 def ang_to_cart(az : Union[float, np.ndarray] , el : Union[float, np.ndarray], unit: str ="DEG") -> np.ndarray:
