420d5827d1a343f46df18a8786e14dd8b1e9a61f
Jan Kiene <jankiene@mailbox.org>
2021-04-02 21:51:07 +0200

    add VbapPanner class with gain calculation

--- pyvbap/vbap.py
+++ pyvbap/vbap.py
@@ -3,48 +3,59 @@ Implementation of the VBAP method for panning a source in (almost) arbitrary
 speaker setups.
 """
 import numpy as np
+from numpy.typing import ArrayLike
+from typing import Union
 
 
 DEG_2_RAD = np.pi / 180
 
+class VbapPanner:
 
-def pan_2d(az, s, spkr_pos, base_vecs, vol_norm=1):
+    def __init__(self, ls_az : ArrayLike, ls_el : ArrayLike):
+        self.ls_az = np.asarray(ls_az, dtype=float)
+        self.ls_el = np.asarray(ls_el, dtype=float)
+        self.ls_vec = ang_to_cart(self.ls_az, self.ls_el)
+
+    def calc_gains(self, az: float, el: float, triplet_base: np.ndarray) -> np.ndarray:
+        """
+        Calculate gains for a given loudspeaker triplet to position a source at the 
+        given azimuth and elevation.
+
+        az: azimuth angle in degrees
+        el: elevation angle in degrees
+        triplet_base: 3x3 matrix containing the loudspeaker vectors in its columns (!)
+        """
+        if (b_shape := triplet_base.shape) != (3, 3):
+            msg = f"Base for active triplet has to be 3D, but has shape {b_shape}"
+            raise ValueError(msg)
+
+        source_vec = ang_to_cart(az, el)
+        gains = np.linalg.inv(triplet_base) @ source_vec
+
+        return gains
+
+    def find_active_triangle(self, az: float, el: float):
+        """
+
+        """
+        pass
+
+
+
+def ang_to_cart(az : Union[float, np.ndarray] , el : Union[float, np.ndarray], unit: str ="DEG") -> np.ndarray:
     """
-    Pan the given signal to an azimuth angle in a 2d speaker setup
-
-    az - integer azimuth panning angle
-    s - signal as numpy array
-    spkr_pos - list of the speaker angles (as integers)
-    base_vecs - unit vectors pointing to the speakers in same order (!) as spkr_pos
-    vol_norm - normalisation factor for the computed gains 
+    Calculate three-dimensional unit vector for given azimuth and elevation angles
     """
-    ret = np.zeros((len(s), len(spkr_pos)))
-    if vol_norm == 0:
-        return ret
+    if unit == "DEG":
+        az *= DEG_2_RAD
+        el *= DEG_2_RAD
+    elif unit != "RAD":
+        raise ValueError(f"Unit has to be either 'DEG' or 'RAD', but is {unit}")
 
-    # compute unit vector for given angle
-    rad = az * DEG_2_RAD
-    vec = np.asarray([-np.sin(rad), np.cos(rad)])
-
-    # find active speakers and compute gains
-    spkr_pos = np.asarray(spkr_pos)
-    distance = np.abs(spkr_pos - az)
-    pos_idx = np.arange(len(spkr_pos))
-    active_idx = [x for _, x in sorted(zip(distance, pos_idx))][:2]
-    if distance[active_idx[0]] == 0:
-        active_idx = active_idx[:1]
-        gains = np.asarray([np.sqrt(vol_norm)])
-    else:
-        inv = np.linalg.inv(base_vecs[:, active_idx])
-        gains = _normalize_gains(inv @ vec, vol_norm)
-
-    # return array with shape (len(s), len(spkr_pos))
-    # all columns but those corresponding to the active speakers are zero
-    for i in range(len(active_idx)):
-        tmp = s * gains[i]
-        ret[:, active_idx[i]] = tmp
-
-    return ret
+    x = np.cos(el) * np.cos(az)
+    y = np.cos(el) * np.sin(az)
+    z = np.sin(el)
+    return np.asarray([x, y, z])
 
 
 def _normalize_gains(gains, vol_norm):
@@ -52,10 +63,3 @@ def _normalize_gains(gains, vol_norm):
     Normalize gain factors to garantue power conservation.
     """
     return gains * np.sqrt(vol_norm / np.sum(gains ** 2))
-
-
-def comp_vec_for_angle(ang):
-    """
-    Calculate a vector pointing to the point on a unit circle at angle <ang>
-    """
-    return np.asarray([-np.sin(ang), np.cos(ang)])
